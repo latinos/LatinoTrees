066ff46acf9195a4351ea365e8be8c3ecc1c127d
Andrea <massironi.andrea@gmail.com>
2015-03-12 16:53:23 +0100

    update crab

--- AnalysisStep/test/crab/multicrab.py
+++ AnalysisStep/test/crab/multicrab.py
@@ -41,30 +41,30 @@ if __name__ == '__main__':
    
 
     # samples to be analysed
-
+                   
     config.General.requestName = 'DYll'
     config.Data.inputDataset = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8-tauola_v2/Phys14DR-AVE30BX50_tsg_PHYS14_ST_V1-v1/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
-    config.JobType.pyCfgParams.extend(['label=DYll', 'id=12345', 'scale=1.23'])
+    config.JobType.pyCfgParams.extend(['label=DYll', 'id=12345', 'scale=1.23', 'doNoFilter=True', 'doMuonIsoId=True' , 'doLHE=True', 'doGen=True', 'doBTag=True'])
     submit(config)
 
     config.General.requestName = 'DYll50ns'
     config.Data.inputDataset = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU4bx50_PHYS14_25_V1-v1/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
-    config.JobType.pyCfgParams.extend(['label=DYll50ns', 'id=12345', 'scale=2.13'])
+    config.JobType.pyCfgParams.extend(['label=DYll50ns', 'id=12345', 'scale=2.13', 'doNoFilter=True', 'doMuonIsoId=True' , 'doLHE=True', 'doGen=True', 'doBTag=True'])
     submit(config)
 
     config.General.requestName = 'DYll25ns'
     config.Data.inputDataset = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
-    config.JobType.pyCfgParams.extend(['label=DYll25ns', 'id=12345', 'scale=2.13'])
+    config.JobType.pyCfgParams.extend(['label=DYll25ns', 'id=12345', 'scale=2.13', 'doNoFilter=True', 'doMuonIsoId=True' , 'doLHE=True', 'doGen=True', 'doBTag=True'])
     submit(config)
 
 
     config.General.requestName = 'ggHwwlvlv'
     config.Data.inputDataset = '/GluGluToHToWWTo2LAndTau2Nu_M-125_13TeV-powheg-pythia6/Phys14DR-AVE30BX50_tsg_PHYS14_ST_V1-v1/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
-    config.JobType.pyCfgParams.extend(['label=ggHwwlvlv', 'id=12', 'scale=0.12'])
+    config.JobType.pyCfgParams.extend(['label=ggHwwlvlv', 'id=12', 'scale=0.12', 'doNoFilter=True', 'doMuonIsoId=True' , 'doLHE=True', 'doGen=True', 'doBTag=True'])
     #print " pyCfgParams = ", pyCfgParams
     #print " config.JobType.pyCfgParams = ", config.JobType.pyCfgParams
     submit(config)
