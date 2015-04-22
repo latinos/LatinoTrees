7882193ed3668cc27f6562be721cd3d2b3311d57
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-04-22 16:39:22 +0200

    Collected all datasets

--- AnalysisStep/test/crab/crab_example2.py
+++ AnalysisStep/test/crab/crab_example2.py
@@ -4,15 +4,18 @@ config = Configuration()
 config.section_('General')
 config.General.transferLogs = True
 #config.General.requestName  = 'QCD_Pt-20toInf'
+#config.General.requestName  = 'ZJets_Madgraph'
 #config.General.requestName  = 'WJetsToLNu'
+config.General.requestName  = 'WWTo2L2Nu_Powheg_Spring14'
 #config.General.requestName  = 'TTJets'
 #config.General.requestName  = 'TBarToLeptons_s'
 #config.General.requestName  = 'TToLeptons_s'
 #config.General.requestName  = 'TBarToLeptons_t'
 #config.General.requestName  = 'TToLeptons_t'
-#config.General.requestName  = 'DYJetsToLL'
+#config.General.requestName  = 'HWW125'
+#config.General.requestName  = 'VBF125'
 #config.General.requestName  = 'Higgs_hzpzp_ww_1GeV'
-config.General.requestName  = 'Higgs_hzpzp_ww_100GeV'
+#config.General.requestName  = 'Higgs_hzpzp_ww_100GeV'
 #config.General.requestName  = 'ppTOzh_zTO2v_hTOwwTO2l2v'
 
 config.section_('JobType')
@@ -23,6 +26,7 @@ config.JobType.pyCfgParams = ['label=HWW', 'id=12345', 'scale=1', 'outputFile=st
 
 config.section_('Data')
 #config.Data.inputDataset    = '/QCD_Pt-20toInf_MuEnrichedPt15_PionKaonDecay_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v3/MINIAODSIM'
+config.Data.inputDataset    = '/WWTo2L2Nu_CT10_13TeV-powheg-pythia8-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM'
 #config.Data.inputDataset    = '/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
 #config.Data.inputDataset    = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
 #config.Data.inputDataset    = '/TBarToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
@@ -30,16 +34,18 @@ config.section_('Data')
 #config.Data.inputDataset    = '/TBarToLeptons_t-channel_Tune4C_CSA14_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
 #config.Data.inputDataset    = '/TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
 #config.Data.inputDataset    = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+#config.Data.inputDataset    = '/GluGluToHToWWTo2LAndTau2Nu_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
+#config.Data.inputDataset    = '/VBF_HToWWToLAndTauNuQQ_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
 #config.Data.inputDataset    = '/CRAB_UserFiles/dburns-Higgs_hzpzp_ww_1GeV_13TeV_RECO_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
-config.Data.inputDataset    = '/CRAB_UserFiles/dburns-Higgs_hzpzp_ww_100GeV_13TeV_RECO_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
+#config.Data.inputDataset    = '/CRAB_UserFiles/dburns-Higgs_hzpzp_ww_100GeV_13TeV_RECO_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
 #config.Data.inputDataset    = '/CRAB_UserFiles/dburns-ppTOzh_zTO2v_hTOwwTO2l2v_13TeV_AODSIM_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
-#config.Data.inputDBS        = 'global'
-config.Data.inputDBS        = 'phys03'  # To be used with dburns datasets
+config.Data.inputDBS        = 'global'
+#config.Data.inputDBS        = 'phys03'  # To be used with dburns datasets
 config.Data.splitting       = 'FileBased'
 config.Data.unitsPerJob     = 1
 config.Data.publishDataName = 'NoFilter'
-#config.Data.ignoreLocality  = True
-config.Data.ignoreLocality = False  # To be used with dburns datasets
+config.Data.ignoreLocality  = True
+#config.Data.ignoreLocality = False  # To be used with dburns datasets
 
 config.section_('Site')
 config.Site.storageSite = 'T2_ES_IFCA'
