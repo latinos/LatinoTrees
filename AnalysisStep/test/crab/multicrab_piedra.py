56b318cce8c1ad4834b3272b52454e792524c4f6
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-04-28 14:55:18 +0200

    Completed multicrab list

--- AnalysisStep/test/crab/multicrab_piedra.py
+++ AnalysisStep/test/crab/multicrab_piedra.py
@@ -33,22 +33,102 @@ if __name__ == '__main__':
     def submit(config):
         res = crabCommand('submit', config = config)
 
-    # Samples to be analysed
+    ####################### Public samples to be analysed ######################
                    
-    config.General.requestName = 'QCD_Pt-20toInf'
+    config.General.requestName = 'QCD'
     config.Data.inputDataset   = '/QCD_Pt-20toInf_MuEnrichedPt15_PionKaonDecay_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v3/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
-    config.JobType.pyCfgParams.extend(['label=QCD_Pt-20toInf', 'id=12345', 'scale=79.80534'])
+    config.JobType.pyCfgParams.extend(['label=QCD', 'id=12345', 'scale=79.80534'])
     submit(config)
 
-    config.General.requestName = 'ZJets_Madgraph'
+    config.General.requestName = 'DYJetsToLL'
     config.Data.inputDataset   = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
-    config.JobType.pyCfgParams.extend(['label=ZJets_Madgraph', 'id=12345', 'scale=2.12968'])
+    config.JobType.pyCfgParams.extend(['label=DYJetsToLL', 'id=12345', 'scale=2.12968'])
     submit(config)
 
     config.General.requestName = 'WJetsToLNu'
     config.Data.inputDataset   = '/WJetsToLNu_13TeV-madgraph-pythia8-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
     config.JobType.pyCfgParams = list(pyCfgParams)
     config.JobType.pyCfgParams.extend(['label=WJetsToLNu', 'id=12345', 'scale=2.04731'])
+    submit(config)
+
+    config.General.requestName = 'WWTo2L2Nu'
+    config.Data.inputDataset   = '/WWTo2L2Nu_CT10_13TeV-powheg-pythia8-tauola/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=WWTo2L2Nu', 'id=12345', 'scale=0.01385'])
+    submit(config)
+
+    config.General.requestName = 'TTJets'
+    config.Data.inputDataset   = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=TTJets', 'id=12345', 'scale=0.03269'])
+    submit(config)
+
+    config.General.requestName = 'TBarToLeptons_s'
+    config.Data.inputDataset   = '/TBarToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=TBarToLeptons_s', 'id=12345', 'scale=0.01664'])
+    submit(config)
+
+    config.General.requestName = 'TToLeptons_s'
+    config.Data.inputDataset   = '/TToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=TToLeptons_s', 'id=12345', 'scale=0.01440'])
+    submit(config)
+
+    config.General.requestName = 'TBarToLeptons_t'
+    config.Data.inputDataset   = '/TBarToLeptons_t-channel_Tune4C_CSA14_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=TBarToLeptons_t', 'id=12345', 'scale=0.32380'])
+    submit(config)
+
+    config.General.requestName = 'TToLeptons_t'
+    config.Data.inputDataset   = '/TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=TToLeptons_t', 'id=12345', 'scale=0.27204'])
+    submit(config)
+
+    config.General.requestName = 'HWW125'
+    config.Data.inputDataset   = '/GluGluToHToWWTo2LAndTau2Nu_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=HWW125', 'id=12345', 'scale=0.00996'])
+    submit(config)
+
+    config.General.requestName = 'VBF125'
+    config.Data.inputDataset   = '/VBF_HToWWToLAndTauNuQQ_M-125_13TeV-powheg-pythia6/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=VBF125', 'id=12345', 'scale=0.00089'])
+    submit(config)
+
+    config.General.requestName = 'VH125'
+    config.Data.inputDataset   = '/WH_ZH_HToWW_2Or3WToLNuAndTau_M-125_13TeV_pythia6/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM'
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=VH125', 'id=12345', 'scale=0.00030'])
+    submit(config)
+
+    ####################### Private samples to be analysed #####################
+
+    config.General.requestName = 'Higgs_hzpzp_ww_1GeV'
+    config.Data.inputDataset   = '/CRAB_UserFiles/dburns-Higgs_hzpzp_ww_1GeV_13TeV_RECO_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
+    config.Data.inputDBS       = 'phys03'
+    config.Data.ignoreLocality = False
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=Higgs_hzpzp_ww_1GeV', 'id=12345', 'scale=0.00031'])
+    submit(config)
+
+    config.General.requestName = 'Higgs_hzpzp_ww_100GeV'
+    config.Data.inputDataset   = '/CRAB_UserFiles/dburns-Higgs_hzpzp_ww_100GeV_13TeV_RECO_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
+    config.Data.inputDBS       = 'phys03'
+    config.Data.ignoreLocality = False
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=Higgs_hzpzp_ww_100GeV', 'id=12345', 'scale=0.00031'])
+    submit(config)
+
+    config.General.requestName = 'ppTOzh_zTO2v_hTOwwTO2l2v'
+    config.Data.inputDataset   = '/CRAB_UserFiles/dburns-ppTOzh_zTO2v_hTOwwTO2l2v_13TeV_AODSIM_v1-7d492cb64f2cdaff326f939f96e45c96/USER'
+    config.Data.inputDBS       = 'phys03'
+    config.Data.ignoreLocality = False
+    config.JobType.pyCfgParams = list(pyCfgParams)
+    config.JobType.pyCfgParams.extend(['label=ppTOzh_zTO2v_hTOwwTO2l2v', 'id=12345', 'scale=0.00393'])
     submit(config)
