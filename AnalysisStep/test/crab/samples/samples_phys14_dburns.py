155df74cb3ab7e3091509f90235d4f0312d59baf
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-06-08 10:37:09 +0200

    Started producing Spring15 samples

--- AnalysisStep/test/crab/samples/samples_phys14_dburns.py
+++ AnalysisStep/test/crab/samples/samples_phys14_dburns.py
@@ -1,5 +1,5 @@
 
-# Human readable name          Sample name from DBS                                                                                               Options
+# Latino name                 DAS name                                                                                                         Options
 samples['zTO2v_hwwTO2l2v']  = ['/CRAB_UserFiles/dburns-ppTOzh_zTO2v_hTOwwTO2l2v_13TeV_AODSIM_v1-7d492cb64f2cdaff326f939f96e45c96/USER',        ['label=zTO2v_hwwTO2l2v',  'id=1001', 'scale=5.03e-5']]
 samples['hzpzp_ww_1GeV']    = ['/CRAB_UserFiles/dburns-Higgs_hzpzp_nohdecay_ww_1GeV_13TeV_AODSIM_v1-7d492cb64f2cdaff326f939f96e45c96/USER',    ['label=hzpzp_ww_1GeV',    'id=1002', 'scale=3.19e-5']]
 samples['hzpzp_ww_10GeV']   = ['/CRAB_UserFiles/dburns-Higgs_hzpzp_nohdecay_ww_10GeV_13TeV_AODSIM_v1-7d492cb64f2cdaff326f939f96e45c96/USER',   ['label=hzpzp_ww_10GeV',   'id=1003', 'scale=3.09e-5']]
@@ -11,4 +11,3 @@ samples['hzpzp_ww_1000GeV'] = ['/CRAB_UserFiles/dburns-Higgs_hzpzp_nohdecay_ww_1
 config.Data.inputDBS      = 'phys03'
 config.Data.outLFNDirBase = '/store/user/piedra/test'
 config.Site.storageSite   = 'T2_ES_IFCA'
-
