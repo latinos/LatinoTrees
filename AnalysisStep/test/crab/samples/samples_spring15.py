13fafdb624d1dcf8d9ec42b77e282b2cca1a9147
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-07-01 10:44:22 +0200

    More samples in

--- AnalysisStep/test/crab/samples/samples_spring15.py
+++ AnalysisStep/test/crab/samples/samples_spring15.py
@@ -1,24 +1,27 @@
 ########
 # 25 ns
 ########
-#        CRAB task name               DAS name                                                                                                         Latino name
-samples['25ns_WWTo2L2Nu']         = ['/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                              ['label=WWTo2L2Nu',         'id=00001']]
-samples['25ns_WZ']                = ['/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                       ['label=WZ',                'id=00002']]
-samples['25ns_WZTo3LNu']          = ['/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',          ['label=WZTo3LNu',          'id=00003']]
-samples['25ns_ZZ']                = ['/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM',                       ['label=ZZ',                'id=00004']]
-samples['25ns_TTTo2L2Nu']         = ['/TTTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                              ['label=TTTo2L2Nu',         'id=00005']]
-samples['25ns_WJetsToLNu']        = ['/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',  ['label=WJetsToLNu',        'id=00006']]
-samples['25ns_ggH120ToWWTo2L2Nu'] = ['/GluGluHToWWTo2L2Nu_M120_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=ggH120ToWWTo2L2Nu', 'id=01120']]
+#        CRAB task name               DAS name                                                                                                             Latino name
+samples['25ns_WWTo2L2Nu']         = ['/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                                  ['label=WWTo2L2Nu',         'id=00001']]
+samples['25ns_WZ']                = ['/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                           ['label=WZ',                'id=00002']]
+samples['25ns_WZTo3LNu']          = ['/WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',              ['label=WZTo3LNu',          'id=00003']]
+samples['25ns_ZZ']                = ['/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM',                           ['label=ZZ',                'id=00004']]
+samples['25ns_TTTo2L2Nu']         = ['/TTTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',                                  ['label=TTTo2L2Nu',         'id=00005']]
+samples['25ns_DYJetsToLL']        = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM', ['label=DYJetsToLL',        'id=00006']]
+samples['25ns_WJetsToLNu']        = ['/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15Dr74-Asympt25ns_Mcrun2_74_V9-v1/MINIAODSIM',      ['label=WJetsToLNu',        'id=00007']]
+samples['25ns_ggH120ToWWTo2L2Nu'] = ['/GluGluHToWWTo2L2Nu_M120_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',     ['label=ggH120ToWWTo2L2Nu', 'id=01120']]
 
 
 ########
 # 50 ns
 ########
-#        CRAB task name       DAS name                                                                                    Latino name
-samples['50ns_WWTo2L2Nu'] = ['/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM',        ['label=WWTo2L2Nu', 'id=00001']]
-samples['50ns_WZ']        = ['/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM', ['label=WZ',        'id=00002']]
-samples['50ns_ZZ']        = ['/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM', ['label=ZZ',        'id=00004']]
-samples['50ns_TTTo2L2Nu'] = ['/TTTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM',        ['label=TTTo2L2Nu', 'id=00005']]
+#        CRAB task name        DAS name                                                                                                              Latino name
+samples['50ns_WWTo2L2Nu']  = ['/WWTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM',                                  ['label=WWTo2L2Nu',  'id=00001']]
+samples['50ns_WZ']         = ['/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM',                           ['label=WZ',         'id=00002']]
+samples['50ns_ZZ']         = ['/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM',                           ['label=ZZ',         'id=00004']]
+samples['50ns_TTTo2L2Nu']  = ['/TTTo2L2Nu_13TeV-powheg/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM',                                  ['label=TTTo2L2Nu',  'id=00005']]
+samples['50ns_DYJetsToLL'] = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM', ['label=DYJetsToLL', 'id=00006']]
+samples['50ns_WJetsToLNu'] = ['/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM',      ['label=WJetsToLNu', 'id=00007']]
 
 
 ########
