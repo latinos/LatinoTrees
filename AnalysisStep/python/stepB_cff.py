c813b3addaa435e720f3e2edfd21d65010731ee8
Andrea <massironi.andrea@gmail.com>
2015-07-02 17:10:08 +0200

    jet mass added

--- AnalysisStep/python/stepB_cff.py
+++ AnalysisStep/python/stepB_cff.py
@@ -95,9 +95,10 @@ stepBTree = cms.EDFilter("GenericTreeProducer",
         std_vector_puppijet_eta = cms.string("leadingSecondJetEta"),
         std_vector_puppijet_phi = cms.string("leadingSecondJetPhi"),
 
-        std_vector_jet_pt  = cms.string("leadingJetPt"),
-        std_vector_jet_eta = cms.string("leadingJetEta"),
-        std_vector_jet_phi = cms.string("leadingJetPhi"),
+        std_vector_jet_pt   = cms.string("leadingJetPt"),
+        std_vector_jet_eta  = cms.string("leadingJetEta"),
+        std_vector_jet_phi  = cms.string("leadingJetPhi"),
+        std_vector_jet_mass = cms.string("leadingJetMass"),
 
         mll = cms.string("mll()"),
         ptll = cms.string("pTll()"),
