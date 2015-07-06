cdf38f1f349244850e8bc8d6c9cb36255e0b4544
Andrea <massironi.andrea@gmail.com>
2015-07-06 14:16:00 +0200

    addition QCD electron samples

--- AnalysisStep/test/crab/samples/samples_spring15.py
+++ AnalysisStep/test/crab/samples/samples_spring15.py
@@ -11,8 +11,15 @@ samples['25ns_TTJets']       = ['/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8
 samples['25ns_DYJetsToLL']   = ['/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM',  ['label=DYJetsToLL',   'id=00007']]
 samples['25ns_WJetsToLNu']   = ['/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',       ['label=WJetsToLNu',   'id=00008']]
 samples['25ns_ggHWW120']     = ['/GluGluHToWWTo2L2Nu_M120_13TeV_powheg_JHUgen_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',      ['label=ggHWW120',     'id=01120']]
+
 samples['25ns_QCD15to20Mu']  = ['/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM',   ['label=QCD15to20Mu',  'id=10001']]
 samples['25ns_QCD20toInfMu'] = ['/QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=QCD20toInfMu', 'id=10002']]
+
+samples['25ns_QCD15to20Ele'] = ['/QCD_Pt-15to20_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=QCD15to20Ele', 'id=10003', 'doCut="nLep>0"']]
+samples['25ns_QCD20to30Ele'] = ['/QCD_Pt-20to30_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=QCD20to30Ele', 'id=10004', 'doCut="nLep>0"']]
+samples['25ns_QCD30to50Ele'] = ['/QCD_Pt-30to50_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=QCD30to50Ele', 'id=10005', 'doCut="nLep>0"']]
+samples['25ns_QCD50to80Ele'] = ['/QCD_Pt-50to80_EMEnriched_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=QCD50to80Ele', 'id=10006', 'doCut="nLep>0"']]
+samples['25ns_QCD30toInfDoubleEle'] = ['/QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCUETP8M1_13TeV_Pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM', ['label=QCD30toInfDoubleEle', 'id=10010', 'doCut="nLep>0"']]
 
 
 ########
@@ -35,3 +42,4 @@ samples['50ns_QCD20toInfMu'] = ['/QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13T
 ########
 # config.Data.outLFNDirBase = '/store/user/piedra/test'
 # config.Site.storageSite   = 'T2_ES_IFCA'
+#config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshww/amassiro/RunII/test3Jul/'
