b5aa09ae67f52dfcb0d586e9055baf260a43a462
Andrea <massironi.andrea@gmail.com>
2015-07-13 11:05:49 +0200

    data

---
+++ AnalysisStep/test/crab/samples/samples_dataB.py
@@ -0,0 +1,16 @@
+########
+# 25 ns
+########
+#        CRAB task name          DAS name                                                                                                              Latino name
+samples['SingleElectron']    = ['/SingleElectron/Run2015B-PromptReco-v1/AOD',                                   ['label=DataSE']]
+samples['DoubleEG']           = ['/DoubleEG/Run2015B-PromptReco-v1/AOD',                            ['label=DataDE']]
+
+
+########
+# Alternative global configuration
+########
+# config.Data.outLFNDirBase = '/store/user/piedra/test'
+# config.Site.storageSite   = 'T2_ES_IFCA'
+config.Data.outLFNDirBase = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/'
+
+
