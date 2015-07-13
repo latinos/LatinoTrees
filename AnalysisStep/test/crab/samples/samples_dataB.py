4586c8ba135481cf97ca00f7e3fb7774ba679109
Andrea <massironi.andrea@gmail.com>
2015-07-13 16:47:40 +0200

    update for data

--- AnalysisStep/test/crab/samples/samples_dataB.py
+++ AnalysisStep/test/crab/samples/samples_dataB.py
@@ -4,6 +4,12 @@
 #        CRAB task name          DAS name                                            Latino name
 samples['SingleElectron']    = ['/SingleElectron/Run2015B-PromptReco-v1/MINIAOD',      ['label=SingleElectron']]
 samples['DoubleEG']          = ['/DoubleEG/Run2015B-PromptReco-v1/MINIAOD',            ['label=DoubleEG']]
+samples['DoubleMu']          = ['/DoubleMuon/Run2015B-PromptReco-v1/MINIAOD',          ['label=DoubleMuon']]
+samples['DoubleMuLow']       = ['/DoubleMuonLowMass/Run2015B-PromptReco-v1/MINIAOD',   ['label=DoubleMuon']]
+samples['SingleMu']          = ['/SingleMu/Run2015B-PromptReco-v1/MINIAOD',            ['label=SingleMuon']]
+samples['SingleMuon']        = ['/SingleMuon/Run2015B-PromptReco-v1/MINIAOD',          ['label=SingleMuon']]
+
+
 
 
 ########
@@ -11,10 +17,10 @@ samples['DoubleEG']          = ['/DoubleEG/Run2015B-PromptReco-v1/MINIAOD',     
 ########
 # config.Data.outLFNDirBase = '/store/user/piedra/test'
 # config.Site.storageSite   = 'T2_ES_IFCA'
-config.Data.outLFNDirBase = '/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/'
+config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/'
 
-pyCfgParams += 'globalTag=GR_P_V56'
-pyCfgParams += 'json=testJson'
+pyCfgParams.append('globalTag=GR_P_V56')
+pyCfgParams.append('json=testJson')
 
 config.General.workArea     = 'crab_projects_13July'
 
