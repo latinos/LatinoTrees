57febf4b6fed862ccc3ae3768b976a23a0c0f6f0
Andrea <massironi.andrea@gmail.com>
2015-07-14 12:20:43 +0200

    data

--- AnalysisStep/test/crab/samples/listFiles50ns.py
+++ AnalysisStep/test/crab/samples/listFiles50ns.py
@@ -9,12 +9,12 @@ outputDirectory = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/7Jul/50
 ########
 
 
-samples['WWTo2L2Nu']          = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/WWTo2L2Nu_13TeV-powheg/crab_50ns_WWTo2L2Nu/150706_122845/0000', 'stepB_MC_']
+#samples['WWTo2L2Nu']          = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/WWTo2L2Nu_13TeV-powheg/crab_50ns_WWTo2L2Nu/150706_122845/0000', 'stepB_MC_']
 samples['WZ']                 = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/WZ_TuneCUETP8M1_13TeV-pythia8/crab_50ns_WZ/150706_122750/0000', 'stepB_MC_']
 samples['ZZ']                 = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/ZZ_TuneCUETP8M1_13TeV-pythia8/crab_50ns_ZZ/150706_122619/0000', 'stepB_MC_']
 samples['WJetsToLNu']         = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/crab_50ns_WJetsToLNu/150706_122552/0000', 'stepB_MC_']
-samples['QCD15to20Mu']        = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_50ns_QCD15to20Mu/150706_122836/0000', 'stepB_MC_']
-samples['50ns_QCD20toInfMu']  = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13TeV_pythia8/crab_50ns_QCD20toInfMu/150706_122601/0000', 'stepB_MC_']
+#samples['QCD15to20Mu']        = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/QCD_Pt-15to20_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/crab_50ns_QCD15to20Mu/150706_122836/0000', 'stepB_MC_']
+#samples['QCD20toInfMu']  = ['/store/group/phys_higgs/cmshww/amassiro/RunII/test/QCD_Pt-20toInf_MuEnrichedPt15_TuneCUETP8M1_13TeV_pythia8/crab_50ns_QCD20toInfMu/150706_122601/0000', 'stepB_MC_']
 
 
 
