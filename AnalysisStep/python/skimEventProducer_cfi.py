c3f1bcf4cb25de9e0a86768da6b2aff3184bf340
Andrea <massironi.andrea@gmail.com>
2015-07-15 10:27:28 +0200

    update HLT for Run II

--- AnalysisStep/python/skimEventProducer_cfi.py
+++ AnalysisStep/python/skimEventProducer_cfi.py
@@ -58,50 +58,63 @@ skimEventProducer = cms.EDProducer('SkimEventProducer',
     
     triggerTag = cms.InputTag("TriggerResults","","HLT"),
     singleMuDataPaths = cms.vstring(
-        "1-163261:HLT_Mu15_v*",
-        "163262-165099:HLT_Mu24_v*",
-        "165102-173235:HLT_Mu30_v*",
-        "173236-175972:HLT_Mu40_v*",
-        "175973-180252:HLT_Mu40_eta2p1_v*",
-        "163262-170901:HLT_IsoMu17_v*",
-        "171050-175910:HLT_IsoMu20_v*",
-        "175911-175921:HLT_IsoMu24_v*",
-        "175922-180252:HLT_IsoMu24_eta2p1_v*",
+        #"1-163261:HLT_Mu15_v*",
+        #"163262-165099:HLT_Mu24_v*",
+        #"165102-173235:HLT_Mu30_v*",
+        #"173236-175972:HLT_Mu40_v*",
+        #"175973-180252:HLT_Mu40_eta2p1_v*",
+        #"163262-170901:HLT_IsoMu17_v*",
+        #"171050-175910:HLT_IsoMu20_v*",
+        #"175911-175921:HLT_IsoMu24_v*",
+        #"175922-180252:HLT_IsoMu24_eta2p1_v*",      
 # end of 2011 Data
-"190456-999999:HLT_IsoMu24_eta2p1_v*",
+        #"190456-999999:HLT_IsoMu24_eta2p1_v*",
+# Run II
+        "200000-400000:HLT_IsoMu20_v*",
+        "200000-400000:HLT_IsoTkMu20_v*",
     ),
     doubleMuDataPaths = cms.vstring(
-        "1-165208:HLT_DoubleMu7_v*",
-        "165364-178419:HLT_Mu13_Mu8_v*",
-        "178420-180252:HLT_Mu17_Mu8_v*",
-        "178420-180252:HLT_Mu17_TkMu8_v*",
+        #"1-165208:HLT_DoubleMu7_v*",
+        #"165364-178419:HLT_Mu13_Mu8_v*",
+        #"178420-180252:HLT_Mu17_Mu8_v*",
+        #"178420-180252:HLT_Mu17_TkMu8_v*",
 # end of 2011 Data
-"190456-999999:HLT_Mu17_Mu8_v*",
-"190456-999999:HLT_Mu17_TkMu8_v*",
+        #"190456-999999:HLT_Mu17_Mu8_v*",
+        #"190456-999999:HLT_Mu17_TkMu8_v*",
+# Run II
+        "200000-400000:HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v*",
+        "200000-400000:HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v*",
     ),
     doubleElDataPaths = cms.vstring(
-        "1-170901:HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v*",
-        "171050-180252:HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
+        #"1-170901:HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v*",
+        #"171050-180252:HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
 # end of 2011 Data
-"190456-999999:HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
+        #"190456-999999:HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
+# Run II
+        "200000-400000:HLT_Ele17_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*",
     ),
     muEGDataPaths = cms.vstring(
-        "1-175972:HLT_Mu17_Ele8_CaloIdL_v*",
-        "175973-180252:HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_v*",
-        "1-167913:HLT_Mu8_Ele17_CaloIdL_v*",
-        "167914-180252:HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v*",
+        #"1-175972:HLT_Mu17_Ele8_CaloIdL_v*",
+        #"175973-180252:HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_v*",
+        #"1-167913:HLT_Mu8_Ele17_CaloIdL_v*",
+        #"167914-180252:HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v*",
 # end of 2011 Data
-        "190456-999999:HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
-"190456-999999:HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
+        #"190456-999999:HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
+        #"190456-999999:HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*",
+# Run II
+        "200000-400000:HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v*",
+        "200000-400000:HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v*"
     ),
     singleElDataPaths = cms.vstring(
-        "1-164237:HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*",
-        "165085-166967:HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*",
-        "166968-170901:HLT_Ele52_CaloIdVT_TrkIdT_v*",
-        "170902-178419:HLT_Ele65_CaloIdVT_TrkIdT_v*",
-        "178420-180252:HLT_Ele80_CaloIdVT_TrkIdT_v*",
+        #"1-164237:HLT_Ele27_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*",
+        #"165085-166967:HLT_Ele32_CaloIdVT_CaloIsoT_TrkIdT_TrkIsoT_v*",
+        #"166968-170901:HLT_Ele52_CaloIdVT_TrkIdT_v*",
+        #"170902-178419:HLT_Ele65_CaloIdVT_TrkIdT_v*",
+        #"178420-180252:HLT_Ele80_CaloIdVT_TrkIdT_v*",
 # end of 2011 Data
-"190456-999999:HLT_Ele27_WP80_v*",
+        #"190456-999999:HLT_Ele27_WP80_v*",
+# Run II
+        "200000-400000:HLT_Ele27_eta2p1_WPLoose_Gsf_v1*"
     ),
     AllEmbedPaths = cms.vstring(
         "1-999999:HLT_*",
