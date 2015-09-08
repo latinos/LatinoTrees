80e2f8ef9caa89124971ad5cb219363d68ad097b
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-09-08 13:34:19 +0200

    Added metNoHF

--- AnalysisStep/test/test-run.sh
+++ AnalysisStep/test/test-run.sh
@@ -7,13 +7,13 @@ fi
 
 export EVENTS=$1
 export MYFILE=root://xrootd.unl.edu//store/data/Run2015B/DoubleEG/MINIAOD/17Jul2015-v1/40000/02FC1E69-AF2E-E511-ABA6-0025905B858E.root
-export MYFILE=file:/afs/cern.ch/user/p/piedra/work/store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/50000/68EBC80B-4CFF-E411-8D52-00074305CFFB.root
+#export MYFILE=file:/afs/cern.ch/user/p/piedra/work/store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/50000/68EBC80B-4CFF-E411-8D52-00074305CFFB.root
 
 rm -rf latino_stepB_numEvent${EVENTS}.root
 
 cmsRun stepB.py print       \
-    globalTag=MCRUN2_74_V9A \
-    label=TT                \
+    globalTag=GR_P_V56      \
+    label=DoubleEG          \
     outputFile=stepB.root   \
     selection=LooseNoIso    \
     doNoFilter=False        \
@@ -25,6 +25,10 @@ cmsRun stepB.py print       \
     runPUPPISequence=False  \
     maxEvents=${EVENTS}     \
     inputFiles=${MYFILE}
+
+# For TT
+#   globalTag=MCRUN2_74_V9A \
+#   label=TT                \
 
 # For DoubleEG data
 #   globalTag=GR_P_V56 \
