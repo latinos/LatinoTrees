4d9a0e7bf260726994edd13adec7a41a6c14e5e3
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-09-17 12:36:39 +0200

    Minor changes

--- AnalysisStep/test/crab/samples/samples_dataC_PromptReco_50ns.py
+++ AnalysisStep/test/crab/samples/samples_dataC_PromptReco_50ns.py
@@ -1,12 +1,12 @@
 ########
 # 2015C
 ########
-samples['PromptReco_SingleElectron']    = ['/SingleElectron/Run2015C-PromptReco-v1/MINIAOD',    ['label=SingleElectron']]
-samples['PromptReco_DoubleEG']          = ['/DoubleEG/Run2015C-PromptReco-v1/MINIAOD',          ['label=DoubleEG']]
-samples['PromptReco_DoubleMuon']        = ['/DoubleMuon/Run2015C-PromptReco-v1/MINIAOD',        ['label=DoubleMuon']]
-samples['PromptReco_DoubleMuonLowMass'] = ['/DoubleMuonLowMass/Run2015C-PromptReco-v1/MINIAOD', ['label=DoubleMuon']]
-samples['PromptReco_SingleMuon']        = ['/SingleMuon/Run2015C-PromptReco-v1/MINIAOD',        ['label=SingleMuon']]
-samples['PromptReco_MuonEG']            = ['/MuonEG/Run2015C-PromptReco-v1/MINIAOD',            ['label=MuEG']]
+samples['PromptReco_50ns_SingleElectron']    = ['/SingleElectron/Run2015C-PromptReco-v1/MINIAOD',    ['label=SingleElectron']]
+samples['PromptReco_50ns_DoubleEG']          = ['/DoubleEG/Run2015C-PromptReco-v1/MINIAOD',          ['label=DoubleEG']]
+samples['PromptReco_50ns_DoubleMuon']        = ['/DoubleMuon/Run2015C-PromptReco-v1/MINIAOD',        ['label=DoubleMuon']]
+samples['PromptReco_50ns_DoubleMuonLowMass'] = ['/DoubleMuonLowMass/Run2015C-PromptReco-v1/MINIAOD', ['label=DoubleMuon']]
+samples['PromptReco_50ns_SingleMuon']        = ['/SingleMuon/Run2015C-PromptReco-v1/MINIAOD',        ['label=SingleMuon']]
+samples['PromptReco_50ns_MuonEG']            = ['/MuonEG/Run2015C-PromptReco-v1/MINIAOD',            ['label=MuEG']]
 
 pyCfgParams.append('globalTag=GR_P_V56')
 pyCfgParams.append('is50ns=True')
