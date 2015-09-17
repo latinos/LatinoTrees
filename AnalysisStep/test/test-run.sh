5b42ef5ae3389eb16f7f170434378a729463e1c6
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-09-17 15:04:35 +0200

    puppi on in test-run

--- AnalysisStep/test/test-run.sh
+++ AnalysisStep/test/test-run.sh
@@ -8,11 +8,14 @@ fi
 export EVENTS=$1
 export MYFILE=file:/afs/cern.ch/user/p/piedra/work/store/mc/RunIISpring15DR74/TT_TuneCUETP8M1_13TeV-powheg-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v1/50000/68EBC80B-4CFF-E411-8D52-00074305CFFB.root
 #export MYFILE=root://xrootd.unl.edu//store/data/Run2015B/DoubleEG/MINIAOD/17Jul2015-v1/40000/02FC1E69-AF2E-E511-ABA6-0025905B858E.root
+#export MYFILE=root://xrootd.unl.edu//store/data/Run2015C/SingleElectron/MINIAOD/PromptReco-v1/000/254/307/00000/624082FC-7A45-E511-B404-02163E01246F.root
+#export MYFILE=root://xrootd.unl.edu//store/data/Run2015C/DoubleMuon/MINIAOD/PromptReco-v1/000/254/231/00000/A8A4FD2D-F645-E511-A2CF-02163E0135AD.root
 
 rm -rf latino_stepB_numEvent${EVENTS}.root
 
 cmsRun stepB.py print       \
     is50ns=True             \
+    isPromptRecoData=False  \
     globalTag=MCRUN2_74_V9A \
     label=TT                \
     outputFile=stepB.root   \
@@ -23,7 +26,7 @@ cmsRun stepB.py print       \
     doGen=False             \
     doBTag=True             \
     doLHE=False             \
-    runPUPPISequence=False  \
+    runPUPPISequence=True   \
     maxEvents=${EVENTS}     \
     inputFiles=${MYFILE}
 
