6d4200d2e9484c99ffc1e1e93f60385aeea5924a
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-09-24 11:51:34 +0200

    listFilesData.py update

--- AnalysisStep/test/crab/samples/listFilesData.py
+++ AnalysisStep/test/crab/samples/listFilesData.py
@@ -1,12 +1,19 @@
 ########
 
-outputDirectory = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/7Jul/data/"
+outputDirectory = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/17Sep/data/"
 
-###########
-# Period B
-###########
 
-samples['SingleElectron']    = ['/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/SingleElectron/crab_SingleElectron/150713_144612/0000/', 'stepB_MC_']
-samples['DoubleEG']          = ['/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/DoubleEG/crab_DoubleEG/150713_144632/0000/', 'stepB_MC_']
+########
+# 2015B
+########
 
+### samples['SingleElectron'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/SingleElectron/crab_SingleElectron/150713_144612/0000/', 'stepB_MC_']
+### samples['DoubleEG']       = ['/store/group/phys_higgs/cmshww/amassiro/RunII/13Jul/data/DoubleEG/crab_DoubleEG/150713_144632/0000/',             'stepB_MC_']
+
+
+########
+# 2015C
+########
+
+samples['Run2015C_PromptReco_25ns_DoubleMuonLowMass'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/17Sep/data/DoubleMuonLowMass/crab_Run2015C_PromptReco_25ns_DoubleMuonLowMass/150921_092711/0000/', 'steB_MC_']
 
