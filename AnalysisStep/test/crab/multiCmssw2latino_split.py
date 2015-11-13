d80b9cb181c0e170739ddc24163aca411f309f0a
Jonatan Piedra <jonatan.piedra@gmail.com>
2015-11-13 14:54:05 +0100

    Minor changes

--- AnalysisStep/test/crab/multiCmssw2latino_split.py
+++ AnalysisStep/test/crab/multiCmssw2latino_split.py
@@ -44,9 +44,9 @@ if __name__ == '__main__':
                print "/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select ls " + inputFolder + " | grep " + pattern + " | awk '{ print \"root://eoscms.cern.ch//eos/cms/" + inputFolder + "\"$1 }' > list.txt"
                print "split --lines=20 -d list.txt list_"
                print "for x in `ls list_*` ; do hadd -f `ls $x | awk --field-separator=\"_\" '{ print \"/tmp/" + requestName + "_\"$2\".root\" }'` `cat $x` ; done"
-               print "rm -rf list.txt list_*"
                print "for x in `ls /tmp/" + requestName + "*` ; do python ../cmssw2latino.py $x ; done"
                print "for x in `ls latino_" + requestName + "*` ; do /afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select cp $x " + outputDirectory + " ; done"          
+               print "rm list.txt list_*"
                print "rm latino_" + requestName + "*.root /tmp/" + requestName + "*.root "     
 
            # really execute instructions
@@ -56,9 +56,9 @@ if __name__ == '__main__':
                os.system("/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select ls " + inputFolder + " | grep " + pattern + " | awk '{ print \"root://eoscms.cern.ch//eos/cms/" + inputFolder + "\"$1 }' > list.txt")
                os.system("split --lines=20 -d list.txt list_")
                os.system("for x in `ls list_*` ; do hadd -f `ls $x | awk --field-separator=\"_\" '{ print \"/tmp/" + requestName + "_\"$2\".root\" }'` `cat $x` ; done")
-               os.system("rm -rf list.txt list_*")
                os.system("for x in `ls /tmp/" + requestName + "*` ; do python ../cmssw2latino.py $x ; done")
                os.system("for x in `ls latino_" + requestName + "*` ; do /afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select cp $x " + outputDirectory + " ; done")
+               os.system("rm list.txt list_*")
                os.system("rm latino_" + requestName + "*.root /tmp/" + requestName + "*.root ")
 
     # error
