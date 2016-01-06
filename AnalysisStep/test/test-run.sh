efcf2d316a87566324e49eadcd7a124529e9b14f
amassiro <massironi.andrea@gmail.com>
2016-01-06 18:45:51 +0100

    wow ... now indexes are saved ... and it should be faster

--- AnalysisStep/test/test-run.sh
+++ AnalysisStep/test/test-run.sh
@@ -11,37 +11,37 @@ export EVENTS=$1
 # export MYFILE=file:/afs/cern.ch/work/p/piedra/public/store/data/Run2015D/DoubleMuon/MINIAOD/PromptReco-v4/000/258/159/00000/28A6C3C3-6F6C-E511-94A4-02163E01459B.root
 #export MYFILE=file:/afs/cern.ch/work/p/piedra/public/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v4/000/258/175/00000/FA1AB2EB-436D-E511-8DF2-02163E011E2B.root
 
-# export MYFILE=file:/afs/cern.ch/user/a/amassiro/public/xLatinos/76X/CC3AD3BC-E898-E511-8707-0025904C5182.root
-# 
-# 
-# rm -rf latino_stepB_numEvent${EVENTS}.root
-# 
-# cmsRun stepB.py print                   \
-#     is50ns=False                        \
-#     isPromptRecoData=False              \
-#     globalTag=76X_mcRun2_asymptotic_v12 \
-#     label=ggH                           \
-#     outputFile=stepB.root               \
-#     selection=LooseNoIso                \
-#     doNoFilter=False                    \
-#     doMuonIsoId=True                    \
-#     doEleIsoId=True                     \
-#     doPhotonID=True                     \
-#     doGen=True                          \
-#     doBTag=True                         \
-#     doFatJet=True                       \
-#     doLHE=True                          \
-#     doMCweights=True                    \
-#     maxEvents=${EVENTS}                 \
-#     inputFiles=${MYFILE}
-# 
-#     
-# # # #                     doNoFilter=True \
-# #                     LHEweightSource=externalLHEProducer \
-#                     
-# python cmssw2latino.py stepB_numEvent${EVENTS}.root
-# 
-# rm -rf stepB_numEvent${EVENTS}.root
+export MYFILE=file:/afs/cern.ch/user/a/amassiro/public/xLatinos/76X/CC3AD3BC-E898-E511-8707-0025904C5182.root
+
+
+rm -rf latino_stepB_numEvent${EVENTS}.root
+
+cmsRun stepB.py print                   \
+    is50ns=False                        \
+    isPromptRecoData=False              \
+    globalTag=76X_mcRun2_asymptotic_v12 \
+    label=ggH                           \
+    outputFile=stepB.root               \
+    selection=LooseNoIso                \
+    doNoFilter=False                    \
+    doMuonIsoId=True                    \
+    doEleIsoId=True                     \
+    doPhotonID=True                     \
+    doGen=True                          \
+    doBTag=True                         \
+    doFatJet=True                       \
+    doLHE=True                          \
+    doMCweights=True                    \
+    maxEvents=${EVENTS}                 \
+    inputFiles=${MYFILE}
+
+    
+# # #                     doNoFilter=True \
+#                     LHEweightSource=externalLHEProducer \
+                    
+python cmssw2latino.py stepB_numEvent${EVENTS}.root
+
+rm -rf stepB_numEvent${EVENTS}.root
 
 
 #####
