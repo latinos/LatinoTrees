bee9e097d8260ca0ca1bcd939a93b14eea35c4ce
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-01-08 15:32:35 +0100

    Updated run ranges

--- AnalysisStep/test/crab/samples/samples_dataD_PromptReco_25ns_21OctBis_0851pb.py
+++ AnalysisStep/test/crab/samples/samples_dataD_PromptReco_25ns_21OctBis_0851pb.py
@@ -11,11 +11,8 @@ pyCfgParams.append('globalTag=74X_dataRun2_v5')
 pyCfgParams.append('is50ns=False')
 pyCfgParams.append('isPromptRecoData=True')
 
-# 1280.23/pb
-#config.Data.lumiMask      = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258750_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
-# 2136.361/pb
 config.Data.lumiMask      = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
-config.Data.runRange      = '258159-999999'
+config.Data.runRange      = '258751-999999'
 config.Data.splitting     = 'LumiBased'
 config.Data.unitsPerJob   = 10
 config.Data.outLFNDirBase = '/store/group/phys_higgs/cmshww/amassiro/RunII/21Oct/data/25ns/'
