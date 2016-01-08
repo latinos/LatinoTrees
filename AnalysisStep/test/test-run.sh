0337aa4d3419810199c54c8989dd768044ca168e
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-01-08 11:00:46 +0100

    Minor changes

--- AnalysisStep/test/test-run.sh
+++ AnalysisStep/test/test-run.sh
@@ -7,12 +7,12 @@ fi
 
 export EVENTS=$1
 
-#export MYFILE=root://xrootd.unl.edu//store/data/Run2015D/DoubleMuon/MINIAOD/PromptReco-v4/000/258/159/00000/0C6D4AB0-6F6C-E511-8A64-02163E0133CD.root
-# export MYFILE=file:/afs/cern.ch/work/p/piedra/public/store/data/Run2015D/DoubleMuon/MINIAOD/PromptReco-v4/000/258/159/00000/28A6C3C3-6F6C-E511-94A4-02163E01459B.root
-#export MYFILE=file:/afs/cern.ch/work/p/piedra/public/store/data/Run2015D/DoubleEG/MINIAOD/PromptReco-v4/000/258/175/00000/FA1AB2EB-436D-E511-8DF2-02163E011E2B.root
+
+#####
+### MC example
+#####
 
 export MYFILE=file:/afs/cern.ch/user/a/amassiro/public/xLatinos/76X/CC3AD3BC-E898-E511-8707-0025904C5182.root
-
 
 rm -rf latino_stepB_numEvent${EVENTS}.root
 
@@ -37,7 +37,7 @@ cmsRun stepB.py print                   \
     maxEvents=${EVENTS}                 \
     inputFiles=${MYFILE}
 
-    
+
 # # #                     doNoFilter=True \
 #                     LHEweightSource=externalLHEProducer \
                     
@@ -47,7 +47,7 @@ rm -rf stepB_numEvent${EVENTS}.root
 
 
 #####
-### a data example
+### Data example
 #####
 
 export MYFILE=file:/afs/cern.ch/user/a/amassiro/public/xLatinos/76X/DATA_MuEG_1ED2F05D-E8AF-E511-BDF3-0002C94D575E.root
