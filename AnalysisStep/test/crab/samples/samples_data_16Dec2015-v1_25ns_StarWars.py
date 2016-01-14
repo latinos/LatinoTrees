6d037c5bb300e8e850f9dad88ad5a762a9a103c8
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-01-14 20:17:20 +0100

    Updated data samples

--- AnalysisStep/test/crab/samples/samples_data_16Dec2015-v1_25ns_StarWars.py
+++ AnalysisStep/test/crab/samples/samples_data_16Dec2015-v1_25ns_StarWars.py
@@ -2,26 +2,26 @@
 #
 # Latino StarWars (08Jan) production of Run2015 25ns data
 #
-# Reading 16Dec2015-v1 processing, produced with CMSSW 7_6_3
+# Reading 16Dec2015-v* processing, produced with CMSSW 7_6_3
 #
 # DAS query: dataset=/*/*16Dec2015*/MINIAOD status=VALID
 # https://cmsweb.cern.ch/das/request?view=list&limit=50&instance=prod%2Fglobal&input=dataset+dataset%3D%2F*%2F*16Dec2015*%2FMINIAOD+status%3DVALID
 #
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-samples['DoubleEG_Run2015C_25ns-16Dec2015-v1'] = ['/DoubleEG/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=DoubleEG']]
-#samples['DoubleEG_Run2015D_25ns-16Dec2015-v1'] = ['/DoubleEG/Run2015D-16Dec2015-v1/MINIAOD',      ['label=DoubleEG']]  # PRODUCTION
+samples['DoubleEG_Run2015C_25ns-16Dec2015-v1']  = ['/DoubleEG/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=DoubleEG']]
+#samples['DoubleEG_Run2015D_25ns-16Dec2015-v1'] = ['/DoubleEG/Run2015D-16Dec2015-v2/MINIAOD',      ['label=DoubleEG']]  # PRODUCTION
 
 #samples['DoubleMuon_Run2015C_25ns-16Dec2015-v1'] = ['/DoubleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=DoubleMuon']]  # PRODUCTION
-samples['DoubleMuon_Run2015D_25ns-16Dec2015-v1'] = ['/DoubleMuon/Run2015D-16Dec2015-v1/MINIAOD',      ['label=DoubleMuon']]
+samples['DoubleMuon_Run2015D_25ns-16Dec2015-v1']  = ['/DoubleMuon/Run2015D-16Dec2015-v1/MINIAOD',      ['label=DoubleMuon']]
 
-samples['MuonEG_Run2015C_25ns-16Dec2015-v1'] = ['/MuonEG/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=MuonEG']]
+samples['MuonEG_Run2015C_25ns-16Dec2015-v1']  = ['/MuonEG/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=MuonEG']]
 #samples['MuonEG_Run2015D_25ns-16Dec2015-v1'] = ['/MuonEG/Run2015D-16Dec2015-v1/MINIAOD',      ['label=MuonEG']]  # PRODUCTION
 
-samples['SingleElectron_Run2015C_25ns-16Dec2015-v1'] = ['/SingleElectron/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=SingleElectron']]
+samples['SingleElectron_Run2015C_25ns-16Dec2015-v1']  = ['/SingleElectron/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=SingleElectron']]
 #samples['SingleElectron_Run2015D_25ns-16Dec2015-v1'] = ['/SingleElectron/Run2015D-16Dec2015-v1/MINIAOD',      ['label=SingleElectron']]  # PRODUCTION
 
-samples['SingleMuon_Run2015C_25ns-16Dec2015-v1'] = ['/SingleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=SingleMuon']]
+samples['SingleMuon_Run2015C_25ns-16Dec2015-v1']  = ['/SingleMuon/Run2015C_25ns-16Dec2015-v1/MINIAOD', ['label=SingleMuon']]
 #samples['SingleMuon_Run2015D_25ns-16Dec2015-v1'] = ['/SingleMuon/Run2015D-16Dec2015-v1/MINIAOD',      ['label=SingleMuon']]  # PRODUCTION
 
 pyCfgParams.append('globalTag=76X_dataRun2_v15')
