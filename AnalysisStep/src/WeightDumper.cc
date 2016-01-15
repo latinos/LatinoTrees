fc50dc0090ab5b16de75e630c26339d5c80ad07e
NTrevisa <n.trevisani@campus.unimib.it>
2016-01-15 20:08:47 +0100

    cannot save histogram

--- AnalysisStep/src/WeightDumper.cc
+++ AnalysisStep/src/WeightDumper.cc
@@ -97,23 +97,21 @@ private:
  edm::EDGetTokenT<LHEEventProduct> productLHET_ ;
  edm::EDGetTokenT<GenEventInfoProduct> GenInfoT_ ;
  edm::EDGetTokenT<LHERunInfoProduct> mcLHERunInfoT_ ;
- 
- 
- 
- 
- 
+  
  TTree* myTree_;
  
+ TH1F* _mcWeightExplained; 
  TH1F* _mcWeightPos;
  TH1F* _mcWeightNeg;
  TH1F* _list_vectors_weights;
+
  int _MAXWEIGHTS;
  
  //---- MC qcd scale
  std::vector <double> _weightsLHE;
-  std::vector <std::string> _weightsLHEID;
-  std::vector <std::string> _weightsLHEIDExplained;
-  int list_size;
+ std::vector <std::string> _weightsLHEID;
+  // std::vector <std::string> _weightsLHEIDExplained;
+ int list_size;
  double _weightNominalLHE;
  std::vector <double> _weights;
  double _weightSM;
@@ -146,6 +144,7 @@ WeightDumper::WeightDumper(const edm::ParameterSet& iConfig)
  
  edm::Service<TFileService> fs ;
  _MAXWEIGHTS = 200;
+ _mcWeightExplained = fs -> make <TH1F>("mcWeightExplained", "mcWeightExplained", 10000, 0, 10000);
  _mcWeightPos = fs -> make <TH1F>("mcWeightPos","mcWeightPos", 1, 0, 1);
  _mcWeightNeg = fs -> make <TH1F>("mcWeightNeg","mcWeightNeg", 1, 0, 1);
  _list_vectors_weights = fs -> make <TH1F>("list_vectors_weights","list_vectors_weights", _MAXWEIGHTS, 0, _MAXWEIGHTS);
@@ -155,12 +154,9 @@ WeightDumper::WeightDumper(const edm::ParameterSet& iConfig)
  
  myTree_ -> Branch("weightsLHE", "std::vector<double>", &_weightsLHE);
  myTree_ -> Branch("weightsLHEID", "std::vector<std::string>", &_weightsLHEID);
- myTree_ -> Branch("weightsLHEIDExplained", "std::vector<std::string>", &_weightsLHEIDExplained);
  myTree_ -> Branch("list_size", &list_size);
-//  myTree_ -> Branch("weightsLHE", &_weightsLHE);
  myTree_ -> Branch("weightNominalLHE", &_weightNominalLHE, "weightNominalLHE/D");
  myTree_ -> Branch("weights", "std::vector<double>", &_weights);
-//  myTree_ -> Branch("weights", &_weights);
  myTree_ -> Branch("weightSM", &_weightSM, "weightSM/D");
  
 }
@@ -221,7 +217,7 @@ void WeightDumper::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
    if (_debug) std::cout << " weightLHEID[" << iWeight << "] = " << productLHEHandle->weights()[iWeight].id << std::endl;
  }
 
-
+ /*
  _weightsLHEIDExplained.clear();
  for (unsigned int iWeight = 0; iWeight < num_whichWeightID; iWeight++) {
    int found = 0;
@@ -233,7 +229,8 @@ void WeightDumper::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
    }
    if (found == 0) _weightsLHEIDExplained.push_back("Currently Unknown");
  }
- 
+ */
+
  list_size = list.size();
 
  if (_debug) std::cout << " weightNominalLHE = " << _weightNominalLHE << std::endl;
@@ -247,7 +244,7 @@ void WeightDumper::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
 //   _mcWeightPos->Fill(0.5, _weightSM);
  }
  if (_weightSM < 0) {
-  _mcWeightNeg->Fill(0.5); //---> histogram is filled with +1 event
+   _mcWeightNeg->Fill(0.5); //---> histogram is filled with +1 event
 //   _mcWeightNeg->Fill(0.5, -_weightSM);
  }
  
@@ -277,72 +274,6 @@ WeightDumper::endJob()
 
 // ------------ method called when starting to processes a run  ------------
 void WeightDumper::beginRun(edm::Run const& iRun, edm::EventSetup const&) {
-
-//  FIXME
-//  This doesn't work anymore in 76X. See:
-//  https://hypernews.cern.ch/HyperNews/CMS/get/edmFramework/3319/2/1/1/1/1/1/1/1/1.html
-//  has someone fixed this?
-//  Do we need to run this "text" dumper in older cmssw versions?
-//  How do we know the mapping  "weight" <-> "what is varied" ?
-//  From twiki page?
-//  
-//   edm::Handle<LHERunInfoProduct> run;
-//   //  LHERunInfoProduct        "externalLHEProducer"   ""                "LHE"     
-//   //  edmDumpEventContent  /tmp/amassiro/180BFD9B-CDD0-E411-9330-0CC47A13D09C.root --run 
-//   
-//   typedef std::vector<LHERunInfoProduct::Header>::const_iterator headers_const_iterator;
-//   
-//   //iRun.getByLabel( "externalLHEProducer", run );
-//   //  iRun.getByLabel( mcLHEEventInfoTag_, run );
-//   iRun.getByToken( productLHET_, run );
-//   
-//   LHERunInfoProduct myLHERunInfoProduct = *(run.product());
-//   
-//   for (headers_const_iterator iter=myLHERunInfoProduct.headers_begin(); iter!=myLHERunInfoProduct.headers_end(); iter++){
-//    std::cout << iter->tag() << std::endl;
-//    std::vector<std::string> lines = iter->lines();
-//    for (unsigned int iLine = 0; iLine<lines.size(); iLine++) {
-//     std::cout << lines.at(iLine);
-//    }
-//   }
-//   
-//   
-//   const lhef::HEPRUP thisHeprup_ = run->heprup();
-//   std::cout << "HEPRUP \n" << std::endl;
-//   std::cout << "IDBMUP " << std::setw(14) << std::fixed << thisHeprup_.IDBMUP.first;
-//   std::cout << std::setw(14) << std::fixed << thisHeprup_.IDBMUP.second << std::endl;
-//   std::cout << "EBMUP " << std::setw(14) << std::fixed << thisHeprup_.EBMUP.first;
-//   std::cout << std::setw(14) << std::fixed << thisHeprup_.EBMUP.second << std::endl;
-//   std::cout << "PDFGUP " << std::setw(14) << std::fixed << thisHeprup_.PDFGUP.first;
-//   std::cout << std::setw(14) << std::fixed << thisHeprup_.PDFGUP.second << std::endl;
-//   std::cout << "PDFSUP " << std::setw(14) << std::fixed << thisHeprup_.PDFSUP.first;
-//   std::cout << std::setw(14) << std::fixed << thisHeprup_.PDFSUP.second << std::endl;
-//   std::cout << "IDWTUP " << std::setw(14) << std::fixed << thisHeprup_.IDWTUP << std::endl;
-//   std::cout << "NPRUP " << std::setw(14) << std::fixed << thisHeprup_.NPRUP << std::endl;
-//   std::cout << " XSECUP " << std::setw(14) << std::fixed;
-//   std::cout << " XERRUP " << std::setw(14) << std::fixed;
-//   std::cout << " XMAXUP " << std::setw(14) << std::fixed;
-//   std::cout << " LPRUP " << std::setw(14) << std::fixed << std::endl;
-//   for ( unsigned int iSize = 0 ; iSize < thisHeprup_.XSECUP.size() ; iSize++ ) {
-//    std::cout << std::setw(14) << std::fixed << thisHeprup_.XSECUP[iSize];
-//    std::cout << std::setw(14) << std::fixed << thisHeprup_.XERRUP[iSize];
-//    std::cout << std::setw(14) << std::fixed << thisHeprup_.XMAXUP[iSize];
-//    std::cout << std::setw(14) << std::fixed << thisHeprup_.LPRUP[iSize];
-//    std::cout << std::endl;
-//   }
-//   std::cout << " " << std::endl;
-//  
-/*
-  std::ifstream inFile;
-  inFile.open("weightList.txt", std::ios::in);
-  while (!inFile.eof()){
-    char dumpList[100];
-    inFile.getline(dumpList,100);
-    std::cout<<dumpList<<std::endl;
-    list.push_back(std::string(dumpList));
-  }
-  inFile.close();
-*/
   
   edm::Handle<LHERunInfoProduct> run;
  
@@ -350,29 +281,40 @@ void WeightDumper::beginRun(edm::Run const& iRun, edm::EventSetup const&) {
   
   if (!(mcLHERunInfoTag_ == edm::InputTag(""))) {
     
-    std::cout<<"hola"<<std::endl;
-  
     iRun.getByLabel( mcLHERunInfoTag_, run );
 //     iRun.getByToken( mcLHERunInfoT_, run );
     
-    std::cout<<"hola"<<std::endl;  
     LHERunInfoProduct myLHERunInfoProduct = *(run.product()); 
     
-    std::cout<<"hola"<<std::endl;  
     for (headers_const_iterator iter=myLHERunInfoProduct.headers_begin(); iter!=myLHERunInfoProduct.headers_end(); iter++){
-      std::cout << "iter->tag():";
-      std::cout << iter->tag() << std::endl;
+      //std::cout << "iter->tag():";
+      //std::cout << iter->tag() << std::endl;
       std::vector<std::string> lines = iter->lines();
       for (unsigned int iLine = 0; iLine<lines.size(); iLine++) {
 	if (iter->tag() == "initrwgt"){
 	  TString a = lines.at(iLine);
 	  list.push_back(a);
+	  if (a.IsAlpha()) continue;
+	  std::cout<<a<<std::endl;
+	  if (a.Contains("<weight id=")){
+	    int c = 0;
+	    TString b = a;
+	    b.Remove(0,12);
+	    b.Remove(4,b.Length()-4);
+	    std::cout<<"b = "<<b<<std::endl;
+	    if (b.IsDigit()){
+	      c = b.Atoi();
+	      std::cout<<"c = "<<c<<std::endl;	    
+	      _mcWeightExplained -> GetXaxis() -> SetBinLabel(c, a);
+	      _mcWeightExplained -> Fill(c,1);
+	    }
+	  }
 	}
-	std::cout<<lines.at(iLine);
+	//std::cout<<lines.at(iLine);
       }
     }
   }  
-
+  
 
     /*  
 	const lhef::HEPRUP thisHeprup = run->heprup();
