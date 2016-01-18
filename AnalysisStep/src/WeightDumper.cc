55bcb311e793f5575ceade19254628b4bbcc543e
amassiro <massironi.andrea@gmail.com>
2016-01-18 15:05:37 +0100

    back

--- AnalysisStep/src/WeightDumper.cc
+++ AnalysisStep/src/WeightDumper.cc
@@ -87,7 +87,7 @@ private:
  virtual void endRun(edm::Run const&, edm::EventSetup const&);
  virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
  virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&);
-  
+ 
  // ----------member data ---------------------------
  edm::InputTag mcLHEEventInfoTag_;
  edm::InputTag mcGenEventInfoTag_;
@@ -97,7 +97,7 @@ private:
  edm::EDGetTokenT<LHEEventProduct> productLHET_ ;
  edm::EDGetTokenT<GenEventInfoProduct> GenInfoT_ ;
  edm::EDGetTokenT<LHERunInfoProduct> mcLHERunInfoT_ ;
-  
+ 
  TTree* myTree_;
  
  TH1F* _mcWeightExplained; 
@@ -105,19 +105,19 @@ private:
  TH1F* _mcWeightPos;
  TH1F* _mcWeightNeg;
  TH1F* _list_vectors_weights;
-
+ 
  int _MAXWEIGHTS;
  
  //---- MC qcd scale
  std::vector <double> _weightsLHE;
  std::vector <std::string> _weightsLHEID;
-  // std::vector <std::string> _weightsLHEIDExplained;
+ // std::vector <std::string> _weightsLHEIDExplained;
  int list_size;
  double _weightNominalLHE;
  std::vector <double> _weights;
  double _weightSM;
  std::vector<TString> list;
-
+ 
 };
 
 //
@@ -182,9 +182,9 @@ void WeightDumper::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
  
  
  
-  
+ 
  edm::Handle<GenEventInfoProduct> genEvtInfo;
-//  iEvent.getByLabel( mcGenEventInfoTag_, genEvtInfo );
+ //  iEvent.getByLabel( mcGenEventInfoTag_, genEvtInfo );
  iEvent.getByToken( GenInfoT_, genEvtInfo );
  
  edm::Handle<LHEEventProduct> productLHEHandle;
@@ -199,7 +199,7 @@ void WeightDumper::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
  _weights.clear();
  if (_debug) std::cout << " evtWeights.size() = " << evtWeights.size() << std::endl;
  for (unsigned int iWeight = 0; iWeight < evtWeights.size(); iWeight++) {
-//   if (_debug) std::cout << " evtWeights[" << iWeight << "] = " << evtWeights.at(iWeight) << std::endl;
+  //   if (_debug) std::cout << " evtWeights[" << iWeight << "] = " << evtWeights.at(iWeight) << std::endl;
   _weights.push_back(evtWeights.at(iWeight));
  }
  if (_debug) std::cout << " weightSM = " << _weightSM << std::endl;
@@ -211,49 +211,49 @@ void WeightDumper::analyze(const edm::Event& iEvent, const edm::EventSetup& iSet
   if (_debug) std::cout << " weightLHE[" << iWeight << "] = " << productLHEHandle->weights()[iWeight].wgt << std::endl;
  }
  _weightNominalLHE = productLHEHandle->originalXWGTUP();
-
+ 
  _weightsLHEID.clear();
  unsigned int num_whichWeightID = productLHEHandle->weights().size();
  for (unsigned int iWeight = 0; iWeight < num_whichWeightID; iWeight++) {
-   _weightsLHEID.push_back( productLHEHandle->weights()[iWeight].id ); 
-   if (_debug) std::cout << " weightLHEID[" << iWeight << "] = " << productLHEHandle->weights()[iWeight].id << std::endl;
+  _weightsLHEID.push_back( productLHEHandle->weights()[iWeight].id ); 
+  if (_debug) std::cout << " weightLHEID[" << iWeight << "] = " << productLHEHandle->weights()[iWeight].id << std::endl;
  }
-
+ 
  /*
- _weightsLHEIDExplained.clear();
- for (unsigned int iWeight = 0; iWeight < num_whichWeightID; iWeight++) {
+  _ weightsLHEIDExplained.clear();*
+  for (unsigned int iWeight = 0; iWeight < num_whichWeightID; iWeight++) {
    int found = 0;
    for (unsigned int j = 0; j < list.size(); ++j){
-     if (list.at(j).Contains(productLHEHandle->weights()[iWeight].id)){
-       _weightsLHEIDExplained.push_back(std::string(list.at(j))); 
-       found = 1;
-     }
-   }
-   if (found == 0) _weightsLHEIDExplained.push_back("Currently Unknown");
- }
- */
-
+    if (list.at(j).Contains(productLHEHandle->weights()[iWeight].id)){
+     _weightsLHEIDExplained.push_back(std::string(list.at(j))); 
+     found = 1;
+}
+}
+if (found == 0) _weightsLHEIDExplained.push_back("Currently Unknown");
+}
+*/
+ 
  list_size = list.size();
-
+ 
  if (_debug) std::cout << " weightNominalLHE = " << _weightNominalLHE << std::endl;
  if (_debug) std::cout << " ---------- " << std::endl; 
  
  myTree_->Fill();
-
+ 
  
  if (_weightSM > 0) {
   _mcWeightPos->Fill(0.5); //---> histogram is filled with +1 event
-//   _mcWeightPos->Fill(0.5, _weightSM);
+  //   _mcWeightPos->Fill(0.5, _weightSM);
  }
  if (_weightSM < 0) {
-   _mcWeightNeg->Fill(0.5); //---> histogram is filled with +1 event
-//   _mcWeightNeg->Fill(0.5, -_weightSM);
+  _mcWeightNeg->Fill(0.5); //---> histogram is filled with +1 event
+  //   _mcWeightNeg->Fill(0.5, -_weightSM);
  }
  
  unsigned int min_num;
  if (int(num_whichWeight) > _MAXWEIGHTS) min_num = _MAXWEIGHTS;
  else  min_num = num_whichWeight;
-  
+ 
  for (unsigned int iWeight = 0; iWeight < min_num ; iWeight++) {
   _list_vectors_weights->Fill( iWeight+0.5, productLHEHandle->weights()[iWeight].wgt/productLHEHandle->originalXWGTUP() );
  }
@@ -276,82 +276,81 @@ WeightDumper::endJob()
 
 // ------------ method called when starting to processes a run  ------------
 void WeightDumper::beginRun(edm::Run const& iRun, edm::EventSetup const&) {
-  
-  edm::Handle<LHERunInfoProduct> run;
  
-  typedef std::vector<LHERunInfoProduct::Header>::const_iterator headers_const_iterator;
+ edm::Handle<LHERunInfoProduct> run;
+ 
+ typedef std::vector<LHERunInfoProduct::Header>::const_iterator headers_const_iterator;
+ 
+ if (!(mcLHERunInfoTag_ == edm::InputTag(""))) {
   
-  if (!(mcLHERunInfoTag_ == edm::InputTag(""))) {
-    
-    iRun.getByLabel( mcLHERunInfoTag_, run );
-//     iRun.getByToken( mcLHERunInfoT_, run );
-    
-    LHERunInfoProduct myLHERunInfoProduct = *(run.product()); 
-    
-    for (headers_const_iterator iter=myLHERunInfoProduct.headers_begin(); iter!=myLHERunInfoProduct.headers_end(); iter++){
-      //std::cout << "iter->tag():";
-      //std::cout << iter->tag() << std::endl;
-      std::vector<std::string> lines = iter->lines();
-      int counter = 0;
-      for (unsigned int iLine = 0; iLine<lines.size(); iLine++) {
-	if (iter->tag() == "initrwgt"){
-	  TString a = lines.at(iLine);
-	  list.push_back(a);
-	  if (a.IsAlpha()) continue;
-          if (_debug) std::cout << a << std::endl;
-	  if (a.Contains("<weight id=")){
-	    int c = 0;
-	    TString b = a;
-	    b.Remove(0,12);
-	    b.Remove(4,b.Length()-4);
-            if (_debug) std::cout << "b = " << b << std::endl;
-	    if (b.IsDigit()){
-	      c = b.Atoi();
-              if (_debug) std::cout << "c = " << c <<std::endl;	    
-	      if (c  == 0) continue;
-	      _mcWeightExplained -> GetXaxis() -> SetBinLabel(c, a);
-	      _mcWeightExplained -> Fill(c,1);
-              
-              _mcWeightExplainedOrdered -> GetXaxis() -> SetBinLabel(counter, a);
-              _mcWeightExplainedOrdered -> Fill(counter,1);
-              counter++;
-            }
-	  }
-	}
-	//std::cout<<lines.at(iLine);
+  iRun.getByLabel( mcLHERunInfoTag_, run );
+  //     iRun.getByToken( mcLHERunInfoT_, run );
+  
+  LHERunInfoProduct myLHERunInfoProduct = *(run.product()); 
+  
+  for (headers_const_iterator iter=myLHERunInfoProduct.headers_begin(); iter!=myLHERunInfoProduct.headers_end(); iter++){
+   //std::cout << "iter->tag():";
+   //std::cout << iter->tag() << std::endl;
+   std::vector<std::string> lines = iter->lines();
+   int counter = 0;
+   for (unsigned int iLine = 0; iLine<lines.size(); iLine++) {
+    if (iter->tag() == "initrwgt"){
+     TString a = lines.at(iLine);
+     list.push_back(a);
+     if (a.IsAlpha()) continue;
+     if (_debug) std::cout << a << std::endl;
+     if (a.Contains("<weight id=")){
+      int c = 0;
+      TString b = a;
+      b.Remove(0,12);
+      b.Remove(4,b.Length()-4);
+      if (_debug) std::cout << "b = " << b << std::endl;
+      if (b.IsDigit()){
+       c = b.Atoi();
+       if (_debug) std::cout << "c = " << c <<std::endl;     
+       _mcWeightExplained -> GetXaxis() -> SetBinLabel(c, a);
+       _mcWeightExplained -> Fill(c,1);
+       
+       _mcWeightExplainedOrdered -> GetXaxis() -> SetBinLabel(counter+1, a);
+       _mcWeightExplainedOrdered -> Fill(counter+1,1);
+       counter++;
       }
+     }
     }
-  }  
+    //std::cout<<lines.at(iLine);
+   }
+  }
+ }  
+ 
+ 
+ /*  
+  c onst lhef::HEPRUP thisH*eprup = run->heprup();
   
-
-    /*  
-	const lhef::HEPRUP thisHeprup = run->heprup();
-	
-	std::cout << "HEPRUP \n" << std::endl;
-	std::cout << "IDBMUP " << std::setw(14) << std::fixed << thisHeprup.IDBMUP.first;
-	std::cout << std::setw(14) << std::fixed << thisHeprup.IDBMUP.second << std::endl;
-	std::cout << "EBMUP " << std::setw(14) << std::fixed << thisHeprup.EBMUP.first;
-	std::cout << std::setw(14) << std::fixed << thisHeprup.EBMUP.second << std::endl;
-	std::cout << "PDFGUP " << std::setw(14) << std::fixed << thisHeprup.PDFGUP.first;
-	std::cout << std::setw(14) << std::fixed << thisHeprup.PDFGUP.second << std::endl;
-	std::cout << "PDFSUP " << std::setw(14) << std::fixed << thisHeprup.PDFSUP.first;
-	std::cout << std::setw(14) << std::fixed << thisHeprup.PDFSUP.second << std::endl;
-	std::cout << "IDWTUP " << std::setw(14) << std::fixed << thisHeprup.IDWTUP << std::endl;
-	std::cout << "NPRUP " << std::setw(14) << std::fixed << thisHeprup.NPRUP << std::endl;
-	std::cout << " XSECUP " << std::setw(14) << std::fixed;
-	std::cout << " XERRUP " << std::setw(14) << std::fixed;
-	std::cout << " XMAXUP " << std::setw(14) << std::fixed;
-	std::cout << " LPRUP " << std::setw(14) << std::fixed << std::endl;
-	for ( unsigned int iSize = 0 ; iSize < thisHeprup.XSECUP.size() ; iSize++ ) {
-	std::cout << std::setw(14) << std::fixed << thisHeprup.XSECUP[iSize];
-	std::cout << std::setw(14) << std::fixed << thisHeprup.XERRUP[iSize];
-	std::cout << std::setw(14) << std::fixed << thisHeprup.XMAXUP[iSize];
-	std::cout << std::setw(14) << std::fixed << thisHeprup.LPRUP[iSize];
-	std::cout << std::endl;
-	}
-	std::cout << " " << std::endl;
-    */  
-    //} 
+  std::cout << "HEPRUP \n" << std::endl;
+  std::cout << "IDBMUP " << std::setw(14) << std::fixed << thisHeprup.IDBMUP.first;
+  std::cout << std::setw(14) << std::fixed << thisHeprup.IDBMUP.second << std::endl;
+  std::cout << "EBMUP " << std::setw(14) << std::fixed << thisHeprup.EBMUP.first;
+  std::cout << std::setw(14) << std::fixed << thisHeprup.EBMUP.second << std::endl;
+  std::cout << "PDFGUP " << std::setw(14) << std::fixed << thisHeprup.PDFGUP.first;
+  std::cout << std::setw(14) << std::fixed << thisHeprup.PDFGUP.second << std::endl;
+  std::cout << "PDFSUP " << std::setw(14) << std::fixed << thisHeprup.PDFSUP.first;
+  std::cout << std::setw(14) << std::fixed << thisHeprup.PDFSUP.second << std::endl;
+  std::cout << "IDWTUP " << std::setw(14) << std::fixed << thisHeprup.IDWTUP << std::endl;
+  std::cout << "NPRUP " << std::setw(14) << std::fixed << thisHeprup.NPRUP << std::endl;
+  std::cout << " XSECUP " << std::setw(14) << std::fixed;
+  std::cout << " XERRUP " << std::setw(14) << std::fixed;
+  std::cout << " XMAXUP " << std::setw(14) << std::fixed;
+  std::cout << " LPRUP " << std::setw(14) << std::fixed << std::endl;
+  for ( unsigned int iSize = 0 ; iSize < thisHeprup.XSECUP.size() ; iSize++ ) {
+   std::cout << std::setw(14) << std::fixed << thisHeprup.XSECUP[iSize];
+   std::cout << std::setw(14) << std::fixed << thisHeprup.XERRUP[iSize];
+   std::cout << std::setw(14) << std::fixed << thisHeprup.XMAXUP[iSize];
+   std::cout << std::setw(14) << std::fixed << thisHeprup.LPRUP[iSize];
+   std::cout << std::endl;
+}
+std::cout << " " << std::endl;
+*/  
+ //} 
 }
 
 // ------------ method called when ending the processing of a run  ------------
