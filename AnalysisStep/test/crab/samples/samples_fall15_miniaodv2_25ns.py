4608378b75adfeafd357b720c39937b40d38782c
NTrevisa <n.trevisani@campus.unimib.it>
2016-03-21 17:56:28 +0100

    monoH

--- AnalysisStep/test/crab/samples/samples_fall15_miniaodv2_25ns.py
+++ AnalysisStep/test/crab/samples/samples_fall15_miniaodv2_25ns.py
@@ -352,25 +352,43 @@ samples['ttDM0150pseudo0500'] = ['/TTbarDMJets_pseudoscalar_Mchi-150_Mphi-500_Tu
 # samples['monoH_2HDM_MZp-2500_MA0-300'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2500_MA0-300_13TeV-madgraph/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM', ['label=monoH_2HDM_MZp-2500_MA0-300', 'id=80008', 'doLHE=False']]
 
 # monoHiggs 76X
- samples['monoH_2HDM_MZp-1000_MA0-500'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-500_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-500', 'id=80010', 'doLHE=False']]
- samples['monoH_2HDM_MZp-1000_MA0-800'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-800_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-800', 'id=80011', 'doLHE=False']]
- samples['monoH_2HDM_MZp-2500_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2500_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2500_MA0-600', 'id=80012', 'doLHE=False']]
- samples['monoH_2HDM_MZp-600_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-600_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-600_MA0-400', 'id=80013', 'doLHE=False']]
- samples['monoH_2HDM_MZp-800_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-800_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-800_MA0-600', 'id=80014', 'doLHE=False']]
+samples['monoH_2HDM_MZp-600_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-600_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-600_MA0-400', 'id=80013', 'doLHE=False']]
 
+samples['monoH_2HDM_MZp-800_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-800_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-800_MA0-600', 'id=80014', 'doLHE=False']]
+
+samples['monoH_2HDM_MZp-1000_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-400', 'id=80028', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1000_MA0-500'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-500_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-500', 'id=80010', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1000_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-600', 'id=80015', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1000_MA0-700'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-700_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-700', 'id=80016', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1000_MA0-800'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-800_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1000_MA0-800', 'id=80011', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1200_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-
+
+v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1200_MA0-400', 'id=80029', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1200_MA0-500'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-500_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1200_MA0-500', 'id=80030', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1200_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1200_MA0-600', 'id=80031', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1200_MA0-700'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-700_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1200_MA0-700', 'id=80017', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1200_MA0-800'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-800_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1200_MA0-800', 'id=80018', 'doLHE=False']]
-samples['monoH_2HDM_MZp-1400_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1400_MA0-400', 'id=80019', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1400_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-
+
+v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1400_MA0-400', 'id=80019', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1400_MA0-500'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-500_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1400_MA0-500', 'id=80020', 'doLHE=False']]
-samples['monoH_2HDM_MZp-1700_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1700_MA0-400', 'id=80021', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1400_MA0-700'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-700_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1400_MA0-700', 'id=80032', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1700_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-
+
+v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1700_MA0-400', 'id=80021', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1700_MA0-500'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-500_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1700_MA0-500', 'id=80022', 'doLHE=False']]
+samples['monoH_2HDM_MZp-1700_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1700_MA0-600', 'id=80033', 'doLHE=False']]
 samples['monoH_2HDM_MZp-1700_MA0-700'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-700_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-1700_MA0-700', 'id=80023', 'doLHE=False']]
+
 samples['monoH_2HDM_MZp-2000_MA0-400'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-400_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2000_MA0-400', 'id=80024', 'doLHE=False']]
 samples['monoH_2HDM_MZp-2000_MA0-500'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-500_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2000_MA0-500', 'id=80025', 'doLHE=False']]
 samples['monoH_2HDM_MZp-2000_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2000_MA0-600', 'id=80026', 'doLHE=False']]
+samples['monoH_2HDM_MZp-2000_MA0-700'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-700_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2000_MA0-700', 'id=80034', 'doLHE=False']]
 samples['monoH_2HDM_MZp-2000_MA0-800'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-800_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2000_MA0-800', 'id=80027', 'doLHE=False']]
+
+samples['monoH_2HDM_MZp-2500_MA0-600'] = ['/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2500_MA0-600_13TeV-madgraph/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v2/MINIAODSIM', ['label=monoH_2HDM_MZp-2500_MA0-600', 'id=80012', 'doLHE=False']]
+
+
 
 
 ## WW PS
