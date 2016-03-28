6a93857e7c22f439cb57dc1887e9ebab3241d0d2
NTrevisa <n.trevisani@campus.unimib.it>
2016-03-28 15:16:34 +0200

    more monoH

---
+++ AnalysisStep/test/crab/samples/listFilesMonoH.py
@@ -0,0 +1,45 @@
+########
+
+outputDirectory = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/RunII/03Mar/MC/25ns/LatinoTrees/"
+
+
+
+########
+# MonoH
+########
+
+# samples['monoH_2HDM_MZp-600_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/18Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-600_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-600_MA0-400/160310_122640/0000/', 'stepB_']
+
+# samples['monoH_2HDM_MZp-800_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-800_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-800_MA0-400/160322_141750/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-800_MA0-500'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-800_MA0-500_13TeV-madgraph/crab_monoH_2HDM_MZp-800_MA0-500/160322_142423/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-800_MA0-600']  = ['/store/group/phys_higgs/cmshww/amassiro/RunII/18Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-800_MA0-600_13TeV-madgraph/crab_monoH_2HDM_MZp-800_MA0-600/160310_122730/0000/',   'stepB_']
+ 
+# samples['monoH_2HDM_MZp-1000_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-1000_MA0-400/160322_085718/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1000_MA0-500'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/18Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-500_13TeV-madgraph/crab_monoH_2HDM_MZp-1000_MA0-500/160310_122657/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1000_MA0-600'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-600_13TeV-madgraph/crab_monoH_2HDM_MZp-1000_MA0-600/160318_172230/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1000_MA0-700'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-700_13TeV-madgraph/crab_monoH_2HDM_MZp-1000_MA0-700/160318_172320/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1000_MA0-800'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/18Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1000_MA0-800_13TeV-madgraph/crab_monoH_2HDM_MZp-1000_MA0-800/160310_122622/0000/', 'stepB_']
+
+# samples['monoH_2HDM_MZp-1200_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-1200_MA0-400/160322_085828/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1200_MA0-500'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-500_13TeV-madgraph/crab_monoH_2HDM_MZp-1200_MA0-500/160322_085900/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1200_MA0-600'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-600_13TeV-madgraph/crab_monoH_2HDM_MZp-1200_MA0-600/160322_085739/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1200_MA0-700'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-700_13TeV-madgraph/crab_monoH_2HDM_MZp-1200_MA0-700/160318_172244/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1200_MA0-800'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1200_MA0-800_13TeV-madgraph/crab_monoH_2HDM_MZp-1200_MA0-800/160318_172300/0000/', 'stepB_']
+
+# samples['monoH_2HDM_MZp-1400_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-1400_MA0-400/160318_172410/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1400_MA0-500'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-500_13TeV-madgraph/crab_monoH_2HDM_MZp-1400_MA0-500/160318_172139/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1400_MA0-700'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1400_MA0-700_13TeV-madgraph/crab_monoH_2HDM_MZp-1400_MA0-700/160322_085844/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1400_MA0-800'] = ['', 'stepB_']
+
+# samples['monoH_2HDM_MZp-1700_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-1700_MA0-400/160318_172446/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1700_MA0-500'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-500_13TeV-madgraph/crab_monoH_2HDM_MZp-1700_MA0-500/160318_172338/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1700_MA0-600'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-600_13TeV-madgraph/crab_monoH_2HDM_MZp-1700_MA0-600/160322_085804/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-1700_MA0-700'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-1700_MA0-700_13TeV-madgraph/crab_monoH_2HDM_MZp-1700_MA0-700/160318_172155/0000/', 'stepB_']
+
+# samples['monoH_2HDM_MZp-2000_MA0-400'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-400_13TeV-madgraph/crab_monoH_2HDM_MZp-2000_MA0-400/160318_172353/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-2000_MA0-500'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-500_13TeV-madgraph/crab_monoH_2HDM_MZp-2000_MA0-500/160318_172116/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-2000_MA0-600'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-600_13TeV-madgraph/crab_monoH_2HDM_MZp-2000_MA0-600/160318_172429/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-2000_MA0-700'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-700_13TeV-madgraph/crab_monoH_2HDM_MZp-2000_MA0-700/160322_085702/0000/', 'stepB_']
+# samples['monoH_2HDM_MZp-2000_MA0-800'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/22Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2000_MA0-800_13TeV-madgraph/crab_monoH_2HDM_MZp-2000_MA0-800/160318_172212/0000/', 'stepB_']
+
+# samples['monoH_2HDM_MZp-2500_MA0-600'] = ['/store/group/phys_higgs/cmshww/amassiro/RunII/18Jan/MC/25ns/ZprimeToA0hToA0chichihWWTollnunu_2HDM_MZp-2500_MA0-600_13TeV-madgraph/crab_monoH_2HDM_MZp-2500_MA0-600/160310_122714/0000/', 'stepB_']
