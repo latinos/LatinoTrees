6d7ceabf2803817af16992ccf44103e0c44785da
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-04-13 17:37:43 +0200

    Added test script for CMSSW 80X

---
+++ AnalysisStep/test/test-run-8x.sh
@@ -0,0 +1,34 @@
+if [ $# -lt 1 ]; then
+    echo "  "
+    echo "  ./test-run.sh EVENTS"
+    echo "  "
+    exit -1
+fi
+
+export EVENTS=$1
+
+export MYFILE=root://xrootd.unl.edu//store/relval/CMSSW_8_0_0/DoubleMuon/MINIAOD/80X_dataRun2_relval_v0_RelVal_doubMu2015D-v1/10000/42F381A3-97DA-E511-9077-0CC47A4D76CC.root
+
+rm -rf latino_stepB_data_numEvent${EVENTS}.root
+
+cmsRun stepB.py print                   \
+    reportEvery=10                      \
+    summary=false                       \
+    is50ns=False                        \
+    isPromptRecoData=False              \
+    globalTag=76X_dataRun2_v15          \
+    label=DoubleMuon                    \
+    outputFile=stepB_data.root          \
+    selection=LooseNoIso                \
+    doNoFilter=False                    \
+    doMuonIsoId=True                    \
+    doEleIsoId=True                     \
+    doBTag=False                        \
+    runPUPPISequence=False              \
+    doPhotonID=True                     \
+    maxEvents=${EVENTS}                 \
+    inputFiles=${MYFILE}
+
+python cmssw2latino.py stepB_data_numEvent${EVENTS}.root
+
+rm -rf stepB_data_numEvent${EVENTS}.root
