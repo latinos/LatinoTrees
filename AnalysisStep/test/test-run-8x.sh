0fb6d26deb30704881bba0dc6ed4874baf16549e
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-04-25 09:51:44 +0200

    Minor changes

--- AnalysisStep/test/test-run-8x.sh
+++ AnalysisStep/test/test-run-8x.sh
@@ -7,6 +7,7 @@ fi
 
 export EVENTS=$1
 
+# dataset = /DoubleMuon/CMSSW_8_0_0-80X_dataRun2_relval_v0_RelVal_doubMu2015D-v1/MINIAOD
 export MYFILE=root://xrootd.unl.edu//store/relval/CMSSW_8_0_0/DoubleMuon/MINIAOD/80X_dataRun2_relval_v0_RelVal_doubMu2015D-v1/10000/42F381A3-97DA-E511-9077-0CC47A4D76CC.root
 
 rm -rf latino_stepB_data_numEvent${EVENTS}.root
@@ -15,8 +16,8 @@ cmsRun stepB.py print                   \
     reportEvery=10                      \
     summary=false                       \
     is50ns=False                        \
-    isPromptRecoData=False              \
-    globalTag=76X_dataRun2_v15          \
+    isPromptRecoData=True               \
+    globalTag=80X_mcRun2_asymptotic_v10 \
     label=DoubleMuon                    \
     outputFile=stepB_data.root          \
     selection=LooseNoIso                \
@@ -29,6 +30,6 @@ cmsRun stepB.py print                   \
     maxEvents=${EVENTS}                 \
     inputFiles=${MYFILE}
 
-python cmssw2latino.py stepB_data_numEvent${EVENTS}.root
+### python cmssw2latino.py stepB_data_numEvent${EVENTS}.root
 
-rm -rf stepB_data_numEvent${EVENTS}.root
+### rm -rf stepB_data_numEvent${EVENTS}.root
