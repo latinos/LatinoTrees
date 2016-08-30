7b2395274fa3ad9b214e58fb4d828ddeba134e89
amassiro <massironi.andrea@gmail.com>
2016-08-30 14:17:39 +0200

    new triggers post-ICHEP

--- AnalysisStep/python/skimEventProducer_cfi.py
+++ AnalysisStep/python/skimEventProducer_cfi.py
@@ -266,6 +266,12 @@ skimEventProducer = cms.EDProducer('SkimEventProducer',
 
         # and even higher!
         "HLT_Ele45_WPLoose_Gsf_v*",                                           # 56
+
+
+        # post-ICHEP new triggers
+        "HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v*",              # 57
+        "HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v*",               # 58
+        
         
         ),
 
