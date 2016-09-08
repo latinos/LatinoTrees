ce4285631c2d86820cc32112bf31ada90e57a158
SangEun Lee <d4space@gmail.com>
2016-09-08 23:34:28 +0900

    WZTo3LNu_mllmin01 x-sec

--- AnalysisStep/python/samplesCrossSections.py
+++ AnalysisStep/python/samplesCrossSections.py
@@ -65,6 +65,7 @@ samples['WWTo2L2Nu_CUETUp']	        .extend( ['xsec=12.178',	'kfact=1.000',		're
 samples['WWTo2L2Nu_CUETDown']	        .extend( ['xsec=12.178',	'kfact=1.000',		'ref=E'] )
 samples['WZ']			        .extend( ['xsec=47.130',	'kfact=1.000',		'ref=E'] )
 samples['WZTo3LNu']		        .extend( ['xsec=4.42965',	'kfact=1.000',		'ref=E'] )
+samples['WZTo3LNu_mllmin01']	        .extend( ['xsec=35.2665',	'kfact=1.000',		'ref=X'] ) # From WZTo3LNu comparision
 samples['WZJets']		        .extend( ['xsec=5.2890',	'kfact=1.000',		'ref=E'] ) #https://indico.cern.ch/event/448517/session/0/contribution/16/attachments/1164999/1679225/Long_Generators_WZxsec_05_10_15.pdf
 samples['WZTo2L2Q']		        .extend( ['xsec=5.5950',	'kfact=1.000',		'ref=E'] )
 samples['WZTo1L3Nu']                    .extend( ['xsec=3.033',         'kfact=1.000',          'ref=E'] ) # err 0.00206
