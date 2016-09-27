e11e275431d3740d21129cb2fac8b745d0a370b4
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-09-27 11:27:01 +0200

    Added Run2016G

--- AnalysisStep/test/crab/samples/samples_data_2016_PromptReco.py
+++ AnalysisStep/test/crab/samples/samples_data_2016_PromptReco.py
@@ -15,11 +15,11 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-samples['DoubleEG_Run2016B-PromptReco-v2']       = ['/DoubleEG/Run2016B-PromptReco-v2/MINIAOD',       ['label=DoubleEG']]
-samples['DoubleMuon_Run2016B-PromptReco-v2']     = ['/DoubleMuon/Run2016B-PromptReco-v2/MINIAOD',     ['label=DoubleMuon']]
-samples['MuonEG_Run2016B-PromptReco-v2']         = ['/MuonEG/Run2016B-PromptReco-v2/MINIAOD',         ['label=MuEG']]
-samples['SingleElectron_Run2016B-PromptReco-v2'] = ['/SingleElectron/Run2016B-PromptReco-v2/MINIAOD', ['label=SingleElectron']]
-samples['SingleMuon_Run2016B-PromptReco-v2']     = ['/SingleMuon/Run2016B-PromptReco-v2/MINIAOD',     ['label=SingleMuon']]
+#samples['DoubleEG_Run2016B-PromptReco-v2']       = ['/DoubleEG/Run2016B-PromptReco-v2/MINIAOD',       ['label=DoubleEG']]
+#samples['DoubleMuon_Run2016B-PromptReco-v2']     = ['/DoubleMuon/Run2016B-PromptReco-v2/MINIAOD',     ['label=DoubleMuon']]
+#samples['MuonEG_Run2016B-PromptReco-v2']         = ['/MuonEG/Run2016B-PromptReco-v2/MINIAOD',         ['label=MuEG']]
+#samples['SingleElectron_Run2016B-PromptReco-v2'] = ['/SingleElectron/Run2016B-PromptReco-v2/MINIAOD', ['label=SingleElectron']]
+#samples['SingleMuon_Run2016B-PromptReco-v2']     = ['/SingleMuon/Run2016B-PromptReco-v2/MINIAOD',     ['label=SingleMuon']]
 
 #samples['DoubleEG_Run2016C-PromptReco-v2']       = ['/DoubleEG/Run2016C-PromptReco-v2/MINIAOD',       ['label=DoubleEG']]
 #samples['DoubleMuon_Run2016C-PromptReco-v2']     = ['/DoubleMuon/Run2016C-PromptReco-v2/MINIAOD',     ['label=DoubleMuon']]
@@ -45,10 +45,11 @@ samples['SingleMuon_Run2016B-PromptReco-v2']     = ['/SingleMuon/Run2016B-Prompt
 #samples['SingleElectron_Run2016F-PromptReco-v1'] = ['/SingleElectron/Run2016F-PromptReco-v1/MINIAOD', ['label=SingleElectron']]
 #samples['SingleMuon_Run2016F-PromptReco-v1']     = ['/SingleMuon/Run2016F-PromptReco-v1/MINIAOD',     ['label=SingleMuon']]
 
-
-pyCfgParams.append('globalTag=80X_dataRun2_Prompt_v8')
-pyCfgParams.append('is50ns=False')
-pyCfgParams.append('isPromptRecoData=True')
+samples['DoubleEG_Run2016G-PromptReco-v1']       = ['/DoubleEG/Run2016G-PromptReco-v1/MINIAOD',       ['label=DoubleEG']]
+samples['DoubleMuon_Run2016G-PromptReco-v1']     = ['/DoubleMuon/Run2016G-PromptReco-v1/MINIAOD',     ['label=DoubleMuon']]
+samples['MuonEG_Run2016G-PromptReco-v1']         = ['/MuonEG/Run2016G-PromptReco-v1/MINIAOD',         ['label=MuEG']]
+samples['SingleElectron_Run2016G-PromptReco-v1'] = ['/SingleElectron/Run2016G-PromptReco-v1/MINIAOD', ['label=SingleElectron']]
+samples['SingleMuon_Run2016G-PromptReco-v1']     = ['/SingleMuon/Run2016G-PromptReco-v1/MINIAOD',     ['label=SingleMuon']]
 
 
 ### Jun21 [ Run2016B ] There is v2
@@ -93,28 +94,24 @@ pyCfgParams.append('isPromptRecoData=True')
 # Cert_271036-279116_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt
 # config.Data.runRange = '276831-278808'
 #
+### Sep27_NoL1T [ Run2016G ]
+#
+# pyCfgParams.append('globalTag=80X_dataRun2_v17')
+# Cert_271036-279116_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt
+# config.Data.runRange = '278820-280385'
+#
 
 
-config.Data.lumiMask       = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-279116_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
+pyCfgParams.append('globalTag=80X_dataRun2_v17')
+pyCfgParams.append('is50ns=False')
+pyCfgParams.append('isPromptRecoData=True')
+
+config.Data.lumiMask       = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-280385_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt'
 config.Data.splitting      = 'LumiBased'
 config.Data.unitsPerJob    = 6
-config.Data.outLFNDirBase  = '/store/group/phys_higgs/cmshww/amassiro/RunII/2016/Jul05_v2/data/25ns/'
-config.Data.runRange       = '274444-275125'
+config.Data.outLFNDirBase  = '/store/group/phys_higgs/cmshww/amassiro/RunII/2016/Sep27_NoL1T/data/25ns/'
+config.Data.runRange       = '278820-280385'
 config.JobType.maxMemoryMB = 2500
-
-
-#brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -u /pb -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-277148_13TeV_PromptReco_Collisions16_JSON.txt --hltpath "HLT_Mu17_TrkIsoVVL_v*"
-
-#brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -u /fb -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-277148_13TeV_PromptReco_Collisions16_JSON.txt --hltpath "HLT_Ele45_WPLoose_Gsf_v*"
-
-
-#brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -u /fb -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt --hltpath "HLT_Ele45_WPLoose_Gsf_v*"
-
-
-#brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -u /fb -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt --hltpath "HLT_Ele27_eta2p1_WPLoose_Gsf_v*"
-
-
-#brilcalc lumi -b "STABLE BEAMS" --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json -u /fb -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-276811_13TeV_PromptReco_Collisions16_JSON.txt --hltpath "HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v*"
 
 
 #   ------------------------------
@@ -126,6 +123,7 @@ config.JobType.maxMemoryMB = 2500
 #    Run2016E |   276831 | 277420
 #    Run2016F |   277772 | 278808
 #    Run2016G |   278820 |
+#    Run2016H |   280919 |
 #   ------------------------------
 #
 # https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2016Analysis
