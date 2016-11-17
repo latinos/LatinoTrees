17db352d8ffd056f3083d9c1e80fc0aad63a3955
Jonatan Piedra <jonatan.piedra@gmail.com>
2016-11-17 16:59:29 +0100

    Minor changes

--- AnalysisStep/test/test-run-8x-mc.sh
+++ AnalysisStep/test/test-run-8x-mc.sh
@@ -17,11 +17,8 @@ export EVENTS=$1
 #
 
 
-# dataset = /RelValTTbar_13/CMSSW_8_0_5-PU25ns_80X_mcRun2_asymptotic_v12_gs7120p2-v1/MINIAODSIM
-# export MYFILE=root://eoscms.cern.ch//store/relval/CMSSW_8_0_5/RelValTTbar_13/MINIAODSIM/PU25ns_80X_mcRun2_asymptotic_v12_gs7120p2-v1/00000/C8D488AF-F308-E611-8C6B-0025905A607E.root
-# dataset = /WWTo2L2Nu_13TeV-powheg/RunIISpring16MiniAODv1-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/MINIAODSIM
-#export MYFILE=root://eoscms.cern.ch//store/mc/RunIISpring16MiniAODv1/WWTo2L2Nu_13TeV-powheg/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/50000/7A35BF96-9009-E611-A4DF-0090FAA57E94.root
-export MYFILE=root://eoscms.cern.ch//store/mc/RunIISpring16MiniAODv2/TTTo2L2Nu_13TeV-powheg/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/60000/0005201C-D41B-E611-8E37-002481E0D398.root
+#export MYFILE=root://eoscms.cern.ch//store/mc/RunIISpring16MiniAODv2/TTTo2L2Nu_13TeV-powheg/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0_ext1-v1/60000/0005201C-D41B-E611-8E37-002481E0D398.root
+export MYFILE=root://eoscms.cern.ch//store/mc/RunIISpring16MiniAODv2/GluGluToContinToZZTo4e_13TeV_MCFM701_pythia8/MINIAODSIM/PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/60000/3C5F44A8-D31B-E611-8707-0026B93F48E7.root
 
 rm -rf latino_stepB_mc_numEvent${EVENTS}.root
 
@@ -31,7 +28,7 @@ cmsRun stepB.py print                   \
     is50ns=False                        \
     isPromptRecoData=False              \
     globalTag=80X_mcRun2_asymptotic_v12 \
-    label=TTbar                         \
+    label=ggZZ                          \
     outputFile=stepB_mc.root            \
     selection=LooseNoIso                \
     doNoFilter=False                    \
@@ -41,10 +38,12 @@ cmsRun stepB.py print                   \
     runPUPPISequence=True               \
     doPhotonID=True                     \
     doGen=True                          \
-    doLHE=False                         \
-    doMCweights=False                   \
+    doLHE=True                          \
+    doMCweights=True                    \
     maxEvents=${EVENTS}                 \
-    inputFiles=${MYFILE}
+    inputFiles=${MYFILE}                \
+    LHEweightSource=source              \
+    LHERunInfo=""
 
 python cmssw2latino.py stepB_mc_numEvent${EVENTS}.root
 
